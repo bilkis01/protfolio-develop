ff5f4cbf2f789a16871e821c41114520ea07b509
bilkis01 <bilkiscse@gmail.com>
2022-09-03 17:44:10 +0600

    swiper add

---
+++ index.js
@@ -0,0 +1,14 @@
+// swiper js code
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
